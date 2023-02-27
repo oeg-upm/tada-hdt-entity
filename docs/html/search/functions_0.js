15b2301f45fb60dedeb4d661b325100c995e1fc0
Ahmad Alobaid <ahmad88me@gmail.com>
2023-02-27 04:56:39 +0300

    update docs

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -2,11 +2,13 @@ var searchData=
 [
   ['add_5fedge_0',['add_edge',['../class_graph.html#a017d89d26c38258d428eebd7f6520f4d',1,'Graph::add_edge(TNode *from_node, TNode *to_node)'],['../class_graph.html#ab22459ce06825407f107cd99f089ce4f',1,'Graph::add_edge(string from_uri, string to_uri)']]],
   ['add_5fnode_1',['add_node',['../class_graph.html#a4d248c777857f1a5bc7d91ec71a25ca4',1,'Graph::add_node(string uri)'],['../class_graph.html#a83dff63b3cf6137535e2cb953680602a',1,'Graph::add_node(TNode *tnode)']]],
-  ['annotate_5fcolumn_2',['annotate_column',['../class_entity_ann.html#a73e2e2ea5ca75750360fe288f5c9299a',1,'EntityAnn::annotate_column(std::list&lt; std::list&lt; string &gt; * &gt; *data, unsigned idx)'],['../class_entity_ann.html#ae6cf4d477451bc89c47d61f7f2e2c3e2',1,'EntityAnn::annotate_column(std::list&lt; std::list&lt; string &gt; * &gt; *data, unsigned idx, double alpha)'],['../class_entity_ann.html#a91708dfeb98fc6ef5ea94f292d599f3a',1,'EntityAnn::annotate_column(std::list&lt; std::list&lt; string &gt; * &gt; *data, unsigned idx, bool use_context, bool double_levels)']]],
-  ['annotate_5fentity_5fproperty_5fcolumn_3',['annotate_entity_property_column',['../class_entity_ann.html#afd4cb21e827a362347de6cdfebb2c2cf',1,'EntityAnn']]],
-  ['annotate_5fentity_5fproperty_5fheuristic_4',['annotate_entity_property_heuristic',['../class_entity_ann.html#abe804865569c0eb4c499e45a85a236c6',1,'EntityAnn']]],
-  ['annotate_5fentity_5fproperty_5fpair_5',['annotate_entity_property_pair',['../class_entity_ann.html#ac5bc6d386e30f58cf32f1682304db71b',1,'EntityAnn']]],
-  ['annotate_5fsemi_5fscored_5fcolumn_6',['annotate_semi_scored_column',['../class_entity_ann.html#ae3020633a72b6fb7134f8a2e74ce0a87',1,'EntityAnn::annotate_semi_scored_column(unsigned long m, double alpha)'],['../class_entity_ann.html#add7c4082a9ea6619b7c5e081e7098726',1,'EntityAnn::annotate_semi_scored_column(unsigned long m)']]],
-  ['annotate_5ftext_5fproperty_5fpair_7',['annotate_text_property_pair',['../class_entity_ann.html#a9a31fd628fdd9c1c42cfdbabd9ae228b',1,'EntityAnn']]],
-  ['append_5flabel_5furi_8',['append_label_uri',['../class_entity_ann.html#a0d78ca8e90af761b1b57052675e3532b',1,'EntityAnn']]]
+  ['add_5fproperty_5fcount_2',['add_property_count',['../class_entity_ann.html#aa08cf4892cf8e925204faf340e7aa705',1,'EntityAnn']]],
+  ['annotate_5fcolumn_3',['annotate_column',['../class_entity_ann.html#a73e2e2ea5ca75750360fe288f5c9299a',1,'EntityAnn::annotate_column(std::list&lt; std::list&lt; string &gt; * &gt; *data, unsigned idx)'],['../class_entity_ann.html#ae6cf4d477451bc89c47d61f7f2e2c3e2',1,'EntityAnn::annotate_column(std::list&lt; std::list&lt; string &gt; * &gt; *data, unsigned idx, double alpha)'],['../class_entity_ann.html#a91708dfeb98fc6ef5ea94f292d599f3a',1,'EntityAnn::annotate_column(std::list&lt; std::list&lt; string &gt; * &gt; *data, unsigned idx, bool use_context, bool double_levels)']]],
+  ['annotate_5fentity_5fproperty_5fcolumn_4',['annotate_entity_property_column',['../class_entity_ann.html#afd4cb21e827a362347de6cdfebb2c2cf',1,'EntityAnn']]],
+  ['annotate_5fentity_5fproperty_5fheuristic_5',['annotate_entity_property_heuristic',['../class_entity_ann.html#abe804865569c0eb4c499e45a85a236c6',1,'EntityAnn']]],
+  ['annotate_5fentity_5fproperty_5fpair_6',['annotate_entity_property_pair',['../class_entity_ann.html#ac5bc6d386e30f58cf32f1682304db71b',1,'EntityAnn']]],
+  ['annotate_5fsemi_5fscored_5fcolumn_7',['annotate_semi_scored_column',['../class_entity_ann.html#ae3020633a72b6fb7134f8a2e74ce0a87',1,'EntityAnn::annotate_semi_scored_column(unsigned long m, double alpha)'],['../class_entity_ann.html#add7c4082a9ea6619b7c5e081e7098726',1,'EntityAnn::annotate_semi_scored_column(unsigned long m)']]],
+  ['annotate_5ftext_5fproperty_5fheuristic_8',['annotate_text_property_heuristic',['../class_entity_ann.html#aa2330e7461ecd0f987aa08fbd913226f',1,'EntityAnn']]],
+  ['annotate_5ftext_5fproperty_5fpair_9',['annotate_text_property_pair',['../class_entity_ann.html#a9a31fd628fdd9c1c42cfdbabd9ae228b',1,'EntityAnn']]],
+  ['append_5flabel_5furi_10',['append_label_uri',['../class_entity_ann.html#a0d78ca8e90af761b1b57052675e3532b',1,'EntityAnn']]]
 ];
--- docs/html/search/functions_a.js
+++ docs/html/search/functions_a.js
@@ -1,12 +1,14 @@
 var searchData=
 [
-  ['set_5falpha_0',['set_alpha',['../class_entity_ann.html#a2c6921256787fb5a81a1864f00e8e2a2',1,'EntityAnn']]],
-  ['set_5flanguage_5ftag_1',['set_language_tag',['../class_entity_ann.html#a038f978b3dcb39bb5c0f6c5a5e4d0b54',1,'EntityAnn']]],
-  ['set_5froot_2',['set_root',['../class_graph.html#adb4e0f3eb8b1173360f51a4e17b76220',1,'Graph']]],
-  ['set_5fsample_5fsize_3',['set_sample_size',['../class_entity_ann.html#af74f317e88a233d6df1a4d619a4ccf9b',1,'EntityAnn']]],
-  ['set_5ftitle_5fcase_4',['set_title_case',['../class_entity_ann.html#a0ce8e6ec16975a854d0730ad47da66ad',1,'EntityAnn']]],
-  ['sethdt_5',['setHDT',['../class_entity_ann.html#adfddaf5720e4a8df4630776886afe0bd',1,'EntityAnn']]],
-  ['setlogger_6',['setLogger',['../class_entity_ann.html#afe3f23b3a8a7bf87cb6a7828a6fda360',1,'EntityAnn']]],
-  ['sort_5fpair_5fproperty_7',['sort_pair_property',['../entity_8cpp.html#ac104da45992873ee0917b86c9160e322',1,'entity.cpp']]],
-  ['strip_5fquotes_8',['strip_quotes',['../class_entity_ann.html#a9694b470f9f9881fbf508c541ead30e9',1,'EntityAnn']]]
+  ['search_5fand_5fappend_5frelations_5fwith_5fentities_0',['search_and_append_relations_with_entities',['../class_entity_ann.html#a91e3c0aad5a7aa609f34022dae35bfeb',1,'EntityAnn']]],
+  ['search_5fand_5fappend_5frelations_5fwith_5fobjects_1',['search_and_append_relations_with_objects',['../class_entity_ann.html#a5d467669c517005a59dcf3035e493ac8',1,'EntityAnn']]],
+  ['set_5falpha_2',['set_alpha',['../class_entity_ann.html#a2c6921256787fb5a81a1864f00e8e2a2',1,'EntityAnn']]],
+  ['set_5flanguage_5ftag_3',['set_language_tag',['../class_entity_ann.html#a038f978b3dcb39bb5c0f6c5a5e4d0b54',1,'EntityAnn']]],
+  ['set_5froot_4',['set_root',['../class_graph.html#adb4e0f3eb8b1173360f51a4e17b76220',1,'Graph']]],
+  ['set_5fsample_5fsize_5',['set_sample_size',['../class_entity_ann.html#af74f317e88a233d6df1a4d619a4ccf9b',1,'EntityAnn']]],
+  ['set_5ftitle_5fcase_6',['set_title_case',['../class_entity_ann.html#a0ce8e6ec16975a854d0730ad47da66ad',1,'EntityAnn']]],
+  ['sethdt_7',['setHDT',['../class_entity_ann.html#adfddaf5720e4a8df4630776886afe0bd',1,'EntityAnn']]],
+  ['setlogger_8',['setLogger',['../class_entity_ann.html#afe3f23b3a8a7bf87cb6a7828a6fda360',1,'EntityAnn']]],
+  ['sort_5fpair_5fproperty_9',['sort_pair_property',['../entity_8cpp.html#ac104da45992873ee0917b86c9160e322',1,'entity.cpp']]],
+  ['strip_5fquotes_10',['strip_quotes',['../class_entity_ann.html#a9694b470f9f9881fbf508c541ead30e9',1,'EntityAnn']]]
 ];
